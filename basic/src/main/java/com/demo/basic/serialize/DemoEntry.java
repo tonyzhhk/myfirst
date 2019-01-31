c2576b93070a353e9e852681e0ce61d8c803e678
zhanghaokun <zhanghaokun@jd.com>
2019-01-31 14:43:01 +0800

    文件路径以及try-with-resource语法

--- basic/src/main/java/com/demo/basic/serialize/DemoEntry.java
+++ basic/src/main/java/com/demo/basic/serialize/DemoEntry.java
@@ -6,6 +6,11 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.*;
 
+/**
+ * 序列化demo
+ * @author zhanghk.tony.
+ * @date 2019-01-31 14:22
+ */
 public class DemoEntry {
 
     public static void main(String[] args) {
@@ -14,17 +19,19 @@ public class DemoEntry {
         family.setMammy("Susan");
         family.setSon("Zq");
         System.out.println(JSON.toJSONString(family));
+        // 不定义具体文件路径，文件保存在该路径下
+        System.out.println(System.getProperty("user.dir"));
 
-        ObjectOutputStream oos = null;
-        try {
-            oos = new ObjectOutputStream(new FileOutputStream("tempFile"));
+        // IOUtils.closeQuietly在2.6的时候将它标记为过时，因为Java7里的“try-with-resource”语法的出现
+        // 如下是该语法的实现。在try()内定义的流相当于加了final标记，你无法再改变其内存引用地址即无法再进行赋值操作
+        // windows具体路径"d://tempFile"
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("tempFile"))) {
             oos.writeObject(family);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            IOUtils.closeQuietly(oos);
         }
 
+        // 由于反序列化，会把文件删掉
         File file = new File("tempFile");
         ObjectInputStream ois = null;
         try {
